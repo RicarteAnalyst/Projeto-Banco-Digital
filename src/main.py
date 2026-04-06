0934544e68b9dfcc3f310c18fcd44784806a340b
RicarteAnalyst <nicollasricarte1212@gmail.com>
2026-04-06 18:20:46 -0300

    Feat: Atualização

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from database import get_engine
 
+
 def importar_dados():
     # 1. Carregar o CSV
     print("Lendo o arquivo CSV...")
@@ -10,19 +11,17 @@ def importar_dados():
     print(df.columns)
 
     # 1.1. Checando....
-    #Valores nulos
+    # Valores nulos
     qtd_nulos = df.isnull().sum().sum()
 
-    #Duplicatas
+    # Duplicatas
     qtd_duplicadas = df.duplicated().sum()
 
-    #Negativos
+    # Negativos
     coluna_valor = df['TransactionAmount']
 
     negativos = df[coluna_valor < 0]
     qtd_negativos = len(negativos)
-
-
 
     # --- RELATÓRIO NO TERMINAL ---
     print(f"Relatório de Qualidade:")
@@ -33,7 +32,7 @@ def importar_dados():
     if qtd_negativos > 0:
         print("Atenção: Dados inconsistentes detectados. Limpando...")
         df = df[coluna_valor >= 0]
-    
+
     if qtd_nulos > 0:
         print("Atenção: Dados inconsistentes detectados. Limpando...")
         df = df.dropna()
@@ -42,26 +41,21 @@ def importar_dados():
         print("Atenção: Dados inconsistentes detectados. Limpando...")
         df = df.drop_duplicates()
 
-
-
     # 2. Pequena limpeza (Data Cleaning)
     # Exemplo: remover linhas com valores nulos que podem quebrar o SQL
-    #df_limpo = df.dropna()
-    
+    # df_limpo = df.dropna()
+
     # 3. Conectar ao Banco
     engine = get_engine()
-    
+
     # 4. Enviar para o MySQL
     # Se a tabela 'transacoes' não existir, ele cria. Se existir, ele adiciona (append).
     print("Enviando dados para o MySQL...")
-    #df_limpo.to_sql('transacoes', con=engine, if_exists='append', index=False)
+    # df_limpo.to_sql('transacoes', con=engine, if_exists='append', index=False)
 
     df.to_sql('data_bronze', con=engine, if_exists='replace', index=False)
     print("Sucesso! Dados integrados ao banco.")
 
+
 if __name__ == "__main__":
     importar_dados()
-
-
-
-  
